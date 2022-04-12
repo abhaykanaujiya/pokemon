1bb6203fc53a42c85be10a37727788b73d2569c3
abhaykanaujiya <abhaykanaujiya@gmail.com>
2022-04-13 01:44:04 +0530

    fetch_pokemons

---
+++ pages/pokemon/[id].js
@@ -0,0 +1,62 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import axios from "axios";
+import styles from "../../styles/Details.module.css";
+
+export const getServerSideProps = async ({params}) => {
+  const resp = await axios.get(
+    `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
+  );
+  console.log(resp,"getid");
+  return {
+    props: {
+      pokemon:await resp.data
+    }
+  }
+};
+export default function Details({pokemon}) {
+  return (
+    <div>
+      <Head>
+        <title>{pokemon?.name}</title>
+      </Head>
+      <div>
+        <Link href="/">
+          <a>back to home</a>
+        </Link>
+      </div>
+      <div className={styles.layout}>
+        <div>
+          <img
+            className={styles.picture}
+            src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon?.image}`}
+            alt={pokemon?.name}
+          />
+        </div>
+        <div>
+          <div className={styles.name}>{pokemon?.name}</div>
+          {/* <div className={styles.type}>{pokemon?.type}</div> */}
+          <table>
+            <thead className={styles.header}>
+              <tr>
+                <th>Name</th>
+                <th>Value</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon?.stats?.map(({ name, value }) => (
+                <tr key={name}>
+                  <td className={styles.attribute}>{name}</td>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
